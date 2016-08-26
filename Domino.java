e0b57d7d5e023fac8dacfef10f98371fc814e258
martinfigueroap <martnfigueroa21@gmail.com>
2016-08-26 09:20:18 -0500

    Update Domino.java

--- Domino.java
+++ Domino.java
@@ -1,3 +1,13 @@
 public class Domino{
+  private Ficha[] fichas;
 
+	public Domino()
+	{
+		fichas = new Ficha[28];
+
+		int k = 0;
+		for(int i = 0; i <= 6; i++)
+			for(int j = 0; j <= i; j++, k++)
+				fichas[k] = new Ficha(j, i);
+	}
 }
