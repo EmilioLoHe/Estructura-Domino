6e57a1f16ce90dfa3c0b75b325b7391a77963bd6
anasollanok <anasollanokim@gmail.com>
2016-09-05 09:34:16 -0500

    Update Ficha.java

--- Ficha.java
+++ Ficha.java
@@ -1,4 +1,4 @@
-public class Ficha{
+public class implements Comparable<Ficha>{
   private int lado1, lado2;
 	public Ficha(int lado1, int lado2)
 	{
@@ -15,7 +15,35 @@ public class Ficha{
 	{
 		return lado2;
 	}
-
+	
+	public boolean esMenor(Ficha t){
+		
+		if(this.getLado1() < t.getLado1() && this.getLado2() < t.getLado2()
+		|| this.getLado1() == t.getLado1() && this.getLado2() < t.getLado2())
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean esMayor(Ficha t){
+		
+		if(this.getLado1() > t.getLado1() && this.getLado2() > t.getLado2()
+		|| this.getLado1() == t.getLado1() && this.getLado2() > t.getLado2())
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean esIgual(Ficha t){
+		
+		if(this.getLado1() == t.getLado1() && this.getLado2() == t.getLado2()
+		|| this.getLado1() == t.getLado2() && this.getLado2() == t.getLado1())
+		
+			return true;
+		else
+			return false;
+	}
+	
 	public String toString()
 	{
 		return lado1 + " / " + lado2;
